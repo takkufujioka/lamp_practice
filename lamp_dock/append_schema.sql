d5cb0f1eb9f08709f758cbd491f00882de185f7a
takufujioka <takufujioka1@fujiokatakunoMacBook-Air.local>
2020-10-05 19:59:46 +0900

    #13

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,9 +1,9 @@
 -- テーブルの構造 'history'
 
-CREATE TABLE 'history' (
-  'order_number' int(11) NOT NULL AUTO_INCREMENT,
-  'user_id' int(11) NOT NULL,
-  'purchased' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE `history` (
+  `order_number` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `purchased` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
 
 --
@@ -11,11 +11,11 @@ CREATE TABLE 'history' (
 
 -- テーブルの構造 'details'
 
-CREATE TABLE 'details' (
-  'order_number' int(11) NOT NULL,
-  'name' varchar(100) NOT NULL,
-  'price' int(11) NOT NULL,
-  'amount' int(11) NOT NULL,
+CREATE TABLE `details` (
+  `order_number` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
 );
 
 --
